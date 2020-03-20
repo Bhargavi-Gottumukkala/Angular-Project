3c7deecd139b8d2cd5e82aee161bac1fabe5e3ef
Bhargavi Gottumukkala <bhargavi.gottumukkala@senecaglobal.net>
2020-03-20 14:37:49 +0530

    DI

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,37 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'Tuscan Chicken Skillet',
+            'This is delicious',
+            'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg',
+            [
+                new Ingredient('Noodles', 10),
+                new Ingredient('Chicken', 1)
+            ]),
+        new Recipe(
+            'Garlic Butter Sauteed Zucchini',
+            'Awesome dish',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSh1s99eq3PWSluaEeyJ2XQ3oatcfib_coF95CcRbTnZ6kIO9T_&s',
+            [
+                new Ingredient('Butter', 1),
+                new Ingredient('Garlic', 5)
+            ])
       ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
